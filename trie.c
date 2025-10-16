1f61d9e1af3290100f348c2c7cb091fc0f44ddb4
benita Benjamin <benben29@cs.utexas.edu>
2025-10-16 11:57:28 -0500

    “message”

--- trie.c
+++ trie.c
@@ -18,7 +18,11 @@ void autocomplete(trie_node_t* root, char* prefix) {
     trie_node_t* letter = root;
     char* itr = prefix;
     while (*itr && len < MAX_WORD_LENGTH) {
+       
         letter = letter->children[*itr - FIRST_VALID_CHAR];
+         if (!letter) {
+        return;
+    }
         itr++;
         len++;
     }
